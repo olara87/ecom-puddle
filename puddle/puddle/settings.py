f04bb31edf103ae03f8b2a22b85dab1bcd7f08ca
omar Lara <omarlara1987@gmail.com>
2023-04-13 23:08:37 -0400

    added requirements.txt

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'puddle.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'puddleDB',
+        'USER': 'root',
+        'PASSWORD': 'Centreville@101191',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
